fbb7f9a447fc2da62d4ca31be5647b3a376f758e
electrikbox <electrikbox@icloud.com>
2025-01-07 09:38:59 +0100

    add middleware

--- src/routes/ListRoutes.js
+++ src/routes/ListRoutes.js
@@ -5,12 +5,13 @@ const {
 	deleteItemInList,
 	updateItemInList,
 } = require("../controllers/ListControllers.js");
+const { jwtAuth } = require("../middleWare/jwtAuth");
 
-listRouter.get("/", listAll);
-listRouter.get("/item", );
-listRouter.post("/item", addItemToShopList);
-listRouter.put("/item", updateItemInList);
-listRouter.delete("/item", deleteItemInList);
+listRouter.get("/", jwtAuth, listAll);
+listRouter.get("/item", jwtAuth, );
+listRouter.post("/item", jwtAuth, addItemToShopList);
+listRouter.put("/item", jwtAuth, updateItemInList);
+listRouter.delete("/item", jwtAuth, deleteItemInList);
 
 // Export du module
 // ===========================================================================================
